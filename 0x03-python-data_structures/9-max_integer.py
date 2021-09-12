7aa7724a047b90a33868c226a47551724f32f0ae
underdogs180 <asadirshad38@gmail.com>
2021-09-12 02:08:42 -0700

    Updated file

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    for index in my_list[::-1]:
-        print("{:d}".format(index))
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    biggest = 0
+    for index in my_list:
+        if index > biggest:
+            biggest = index
+    return biggest
