a00601ef77eea2c7b704e5c275c5d53f4a65164a
underdogs180 <asadirshad38@gmail.com>
2021-09-15 07:31:47 -0700

    Modified

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,62 +1,16 @@
 #!/usr/bin/python3
-
-
-def convert_roman(ch):
-        """
-    converts a roman numeral character into the respective integer
-    """
-    ret = -1
-    if ch == 'I':
-        ret = 1
-    elif ch == 'V':
-        ret = 5
-    elif ch == 'X':
-        ret = 10
-    elif ch == 'L':
-        ret = 50
-    elif ch == 'C':
-        ret = 100
-    elif ch == 'D':
-        ret = 500
-    elif ch == 'M':
-        ret = 1000
-    return ret
-
-
-def roamn_to_int(roman_string):
-        """
-        converts any string of roman numerals to decimal
-        """
-        cur_max = -1
-        cur = conv = 0
-        holder = []
-
-        if roman_string is None or type(roman_string) is not str:
-            return 0
-        for c in roman_string:
-            cur = convert_roman(c)
-        if cur == -1:
-            return 0
-        if len(holder) == 0:
-        if cur == cur_max or cur_max == -1:
-            cur_max = cur
-            conv += cur
-        elif cur < cur_max:
-            holder.append(cur)
-        elif cur > cur_max:  # only happens if smaller is starting number
-            # for example: IIX, VXC
-            cur_max = cur
-            cur -= conv
-            conv = cur
-        else:
-            if cur > holder[-1]:
-                cur_max = cur
-                cur -= sum(holder)
-                conv += cur
-                holder.clear()
+def roman_to_int(roman_string):
+    ns = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if ns[roman_string[i]] >= ns[roman_string[i + 1]]:
+                total += ns[roman_string[i]]
             else:
-                holder.append(cur)
-
-    if len(holder) != 0:
-        conv += sum(holder)
-    return conv
+                total -= ns[roman_string[i]]
+            i += 1
+        total += ns[roman_string[i]]
+        return total
+    else:
+        return 0
