f172fdb89e8c9c92db6c2aae6ebbdd448354b50e
underdogs180 <asadirshad38@gmail.com>
2021-09-27 09:24:03 -0700

    Fixed

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,6 +8,6 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except:
-              result = b + a
-              break
-      return result
+            result = b + a
+            break
+    return result
