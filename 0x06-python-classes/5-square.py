04590d43d126677c0c014f0260375dfbaa1ba723
underdogs180 <asadirshad38@gmail.com>
2021-09-28 10:14:22 -0700

    FIXED

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
-class Square:
-    """Square class that calculates the area of
-    a sqare, has a getter and setter method"""
+"""Define a class Square."""
 
-    def __init__(self, size=0):
-        self.__size = size
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of a new square.
+        """
+        self.size = size
 
     @property
-    def size(self):
-        return self.__size
-
-    @size.setter
     def size(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        return self.__size ** 2
+        self.__size = value
 
     def my_print(self):
-        i = 0
-        j = 0
+        """Print the square with the # character."""
+        for i in range(0, self.__size)
+            [print("#", end+"") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
-            print()
-        else:
-            while (i < self.__size):
-                j = 0
-                while (j < self.__size):
-                    print("#", end='')
-                    j += 1
-                print()
-                i += 1
+            print("")
