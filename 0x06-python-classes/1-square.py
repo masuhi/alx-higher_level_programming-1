f5fa6fdfec88adeed941b305505f84ebcd48246b
underdogs180 <asadirshad38@gmail.com>
2021-09-28 05:37:14 -0700

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # 1-square.py
 """Define a class Square."""
-
-
 class Square:
     """Represent a square."""
-
-    def __init__(self, size):
+def __init__(self, size):
         """Initialize a new Square.
-        
         Args:
             size (int): The size of the new square.
         """
