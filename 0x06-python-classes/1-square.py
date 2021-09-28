741373a587d10312ff121a5844a7699117c5ff02
underdogs180 <asadirshad38@gmail.com>
2021-09-28 05:59:53 -0700

    Foxed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-# 1-square.py
-"""Define a class Square."""
 
 
 class Square:
-    """Represent a square."""
-
+        """
+    class square that has attributes:
+        size
+    """
     def __init__(self, size):
-        """Initialize a new Square.
-         
-        Args:
-            size (int): The size of the new square.
+        """
+        the initialization function for the square class
         """
         self.__size = size
