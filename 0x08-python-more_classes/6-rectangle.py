7bca9ed9713be9bcadd56c263a74d1520c642ce3
underdogs180 <asadirshad38@gmail.com>
2021-10-04 11:42:58 -0700

    Modified

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,14 +6,19 @@ Defines a class Rectangle
 
 class Rectangle:
     """Representation of a rectangle"""
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         """prints a string when an instance has been deleted"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
