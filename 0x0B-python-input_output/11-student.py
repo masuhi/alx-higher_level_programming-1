3ea60ffbc8727108bc3140f0825b3f203fc28d23
underdogs180 <asadirshad38@gmail.com>
2021-10-12 08:48:54 -0700

    Modified

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -21,6 +21,5 @@ class Student:
         for a in attrs:
             try:
                 new_dict[a] = self.__dict__[a]
-            except:
-                pass
+            pass
         return new_dict
