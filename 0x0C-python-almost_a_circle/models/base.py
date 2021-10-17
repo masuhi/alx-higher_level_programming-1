478c66764469d19fd9b4c724e06ba369bf592d2c
underdogs180 <asadirshad38@gmail.com>
2021-10-17 07:35:59 -0700

    Mdified

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,21 +19,21 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
-              
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of a list of dictionaries"""
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
-     
+
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
@@ -44,17 +44,17 @@ class Base:
                 lo.append(cls.to_dictionary(i))
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(lo))
-            
+
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        if cls.__name__ is "Rectangle":
+        if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        elif cls.__name__ is "Square":
+        elif cls.__name__ == "Square":
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
-     
+
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
@@ -67,21 +67,21 @@ class Base:
         except:
             pass
         return l
-    
+
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """serializes a list of Rectangles/Squares in csv"""
         filename = cls.__name__ + ".csv"
         with open(filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            if cls.__name__ is "Rectangle":
+            if cls.__name__ == "Rectangle":
                 for obj in list_objs:
                     csv_writer.writerow([obj.id, obj.width, obj.height,
                                          obj.x, obj.y])
-            elif cls.__name__ is "Square":
+            elif cls.__name__ == "Square":
                 for obj in list_objs:
                     csv_writer.writerow([obj.id, obj.size, obj.x, obj.y])
-                    
+
     @classmethod
     def load_from_file_csv(cls):
         """deserializes a list of Rectangles/Squares in csv"""
@@ -91,13 +91,13 @@ class Base:
             with open(filename, 'r') as csvfile:
                 csv_reader = csv.reader(csvfile)
                 for args in csv_reader:
-                    if cls.__name__ is "Rectangle":
+                    if cls.__name__ == "Rectangle":
                         dictionary = {"id": int(args[0]),
                                       "width": int(args[1]),
                                       "height": int(args[2]),
                                       "x": int(args[3]),
                                       "y": int(args[4])}
-                    elif cls.__name__ is "Square":
+                    elif cls.__name__ == "Square":
                         dictionary = {"id": int(args[0]), "size": int(args[1]),
                                       "x": int(args[2]), "y": int(args[3])}
                     obj = cls.create(**dictionary)
@@ -105,7 +105,7 @@ class Base:
         except:
             pass
         return l
-      
+
     @staticmethod
     def draw(list_rectangles, list_squares):
         """opens a window and draws all the Rectangles and Squares"""
@@ -170,7 +170,7 @@ class Base:
                 turtle.right(90)
             turtle.pu()
             turtle.forward(rect.x)
-            turtle.right(90) 
+            turtle.right(90)
             turtle.forwrd(rect.y)
             turtle.left(90)
             turtle.pd()
@@ -229,5 +229,5 @@ class Base:
             turtle.left(90)
             turtle.forward(square.y)
             turtle.right(90)
-            
+
         turtle.getscreen()._root.mainloop()
