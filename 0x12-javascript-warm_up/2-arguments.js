adee0dbd6604858a579d2d0e51c772e75f2a8c37
underdogs180 <asadirshad38@gmail.com>
2021-12-13 12:47:00 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-'use strict';
-let K = process.argv.length;
-if (K === 2) {
-    console.log('No argument');
-} else if (K === 3) {
-    console.log('Argument found');
-} else {
-    console.log('Arguments found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
