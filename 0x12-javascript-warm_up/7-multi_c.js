e5dbe9deca4a7e5c3fd869bd24c68f7c781b0b46
underdogs180 <asadirshad38@gmail.com>
2021-12-13 13:27:23 +0000

    Fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-'use strict';
-let x = process.argv[2];
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
+const x = parseInt(process.argv[2]);
+if (x) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
