814da78d666fbbb3e4301a92888c4fce921755d6
underdogs180 <asadirshad38@gmail.com>
2021-12-13 13:16:38 +0000

    Fixed Error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-'use strict';
-let args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let arg in args) {
-  console.log(args[arg]);
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myStrings.length; i++) {
+  console.log(myStrings[i]);
 }
