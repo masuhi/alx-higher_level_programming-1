534edb93f77abd8cc3eeb1e362143b9b7348a67b
underdogs180 <asadirshad38@gmail.com>
2021-12-19 09:55:20 +0000

    Modified

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-''' script for task 2
-    script should take 4 arguments: mysql username,
-    mysql password, database name and state name searched
-'''
+"""
+python script that lists all states from the database hbtn_0e_0_usa with
+a given name
+"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-
-def list_with_name():
-    ''' displays all values in the states table in hbtn db where name
-        matches the argument passed to the script
-    '''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-    host = 'localhost'
-    port = 3306
-
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name = \'{}\' ORDER BY id ASC;'
-                .format(state_name))
-    result = cur.fetchall()
-    cur.close()
-    db.close()
-
-    if result:
-        for row in result:
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
             print(row)
-
-
-if __name__ == '__main__':
-    list_with_name()
+    cursor.close()
+    db.close()
